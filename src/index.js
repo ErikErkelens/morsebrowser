4a864eafa5fd77bfbadf3d0c3774321d4927f2d6
Erik Erkelens <Erik.Erkelens@gmail.com>
2024-01-28 18:23:38 -0500

    add sw

--- src/index.js
+++ src/index.js
@@ -10,3 +10,13 @@ import './css/style.css'
 import { MorseViewModel } from './morse/morse.ts'
 
 ko.applyBindings(new MorseViewModel())
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration)
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError)
+    })
+  })
+}
